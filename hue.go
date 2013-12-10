76614e8e05f6e167353fdea593baf505fc92eb27
Matt <matt.ho@gmail.com>
2013-12-09 23:59:18 -0800

    - added controls for hue

---
+++ hue.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/savaki/hue"
+	"io"
+	"net/http"
+)
+
+type HueRequest struct {
+	name   string
+	action string
+}
+
+func HueProcessor(username string, ch chan HueRequest) {
+	bridge := hue.NewBridge("10.0.1.11", username)
+	for {
+		request := <-ch
+
+		var lights []*hue.Light = nil
+		switch request.name {
+		case "all":
+			lights, _ = bridge.GetAllLights()
+
+		case "dome":
+			left, err := bridge.FindLightById("3")
+			if err == nil {
+				lights = append(lights, left)
+			}
+
+			right, err := bridge.FindLightById("6")
+			if err == nil {
+				lights = append(lights, right)
+			}
+		}
+
+		if lights != nil {
+			for _, light := range lights {
+				switch request.action {
+				case "on":
+					light.On()
+				case "off":
+					light.Off()
+				case "colorloop":
+					light.ColorLoop()
+				}
+			}
+		}
+	}
+}
+
+func HueHandler(ch chan HueRequest) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, request *http.Request) {
+		vars := mux.Vars(request)
+		name := vars["name"]
+		action := vars["action"]
+
+		ch <- HueRequest{name: name, action: action}
+
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, "hello world")
+	}
+}
--- main.go
+++ main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+	"flag"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
+var (
+	port     = flag.String("port", "8080", "the port number to use")
+	username = flag.String("hue", "", "the hue username")
+)
+
 func main() {
+	flag.Parse()
+
 	router := mux.NewRouter()
 
 	// handle belkin related commands
-	ch := make(chan BelkinRequest, 20)
-	go BelkinProcessor(ch)
-	router.Methods("POST").Subrouter().HandleFunc("/api/belkin/{name}/{action}", BelkinHandler(ch))
+	belkinCh := make(chan BelkinRequest, 20)
+	go BelkinProcessor(belkinCh)
+	router.HandleFunc("/api/belkin/{name}/{action}", BelkinHandler(belkinCh)).Methods("POST")
+
+	// handle hue related commands
+	hueCh := make(chan HueRequest, 20)
+	go HueProcessor(*username, hueCh)
+	router.HandleFunc("/api/hue/{name}/{action}", HueHandler(hueCh)).Methods("POST")
 
 	// default to file server
 	router.Methods("GET").Handler(http.FileServer(http.Dir("public")))
 
-	http.ListenAndServe(":8080", router)
+	http.ListenAndServe(":"+*port, router)
 }
